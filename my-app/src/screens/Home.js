f75e91783902c5e7f61bc8fb810731c3f3051b62
valentina-paz <vpaz@udesa.edu.ar>
2024-06-14 00:50:27 -0300

    empece home

--- my-app/src/screens/Home.js
+++ my-app/src/screens/Home.js
@@ -1,11 +1,42 @@
-import { Text, View } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
 import React, { Component } from 'react'
+import { db } from '../firebase/config'
 
 export default class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            posteos: []
+        }
+    }
+
+    componentDidMount(){
+        db.collection('posteos').onSnapshot((docs) => {
+            let postObtenidos = []
+
+            docs.forEach(doc => {
+                postObtenidos.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+            
+            this.setState({
+                posteos: postObtenidos
+            })
+        })
+    }
+
   render() {
     return (
       <View>
-        <Text>Home</Text>
+        <FlatList
+            data={this.state.posteos}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({item}) => <View>
+                <Text>{item.data.descripcion}</Text>
+            </View>}
+        />
       </View>
     )
   }
