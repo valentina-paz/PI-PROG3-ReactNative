773bb98148ea0a08a720d1384b55e5ce552639d6
antoplati <aplati@udesa.edu.ar>
2024-06-16 21:11:40 -0300

    likeando posteos

--- my-app/src/components/Post.js
+++ my-app/src/components/Post.js
@@ -1,5 +1,8 @@
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 import React, { Component } from 'react'
+import {FontAwesome} from '@expo/vector-icons'
+import { auth, db } from '../firebase/config'
+import firebase from 'firebase'
 
 export default class Post extends Component {
     constructor(props){
@@ -8,13 +11,50 @@ export default class Post extends Component {
             estaMiLike: false,
         }
     }
+
+  componentDidMount(){
+    console.log(this.props)
+    let estaMiLike = this.props.post !== undefined ? this.props.post.data.likes.includes(auth.currentUser.email) : false
+    if(estaMiLike){
+        this.setState({estaMiLike: true})
+    }
+  }
+
+  ponerLike(){
+    db.collection('posteos')
+        .doc(this.props.post.id)
+        .update({
+            likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
+        })
+        .then((resp) => this.setState({estaMiLike: true}))
+        .catch(err => console.log(err))
+  }
+
+  sacarLike(){
+    db.collection('posteos')
+    .doc(this.props.post.id)
+    .update({
+        likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+    })
+    .then((resp) => this.setState({estaMiLike: false}))
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <View>
-        <Text>{
-            this.props.post.data.descripcion
+        <Text>{this.props.post.data.descripcion}</Text>
+        <Text>{this.props.post.data.likes.length}</Text>
+        {
+            this.state.estaMiLike ?
+                <TouchableOpacity onPress={()=> this.sacarLike()}>
+                    <FontAwesome name='heart' size={24} color={'red'} />
+                </TouchableOpacity>
+                :
+                <TouchableOpacity onPress={()=> this.ponerLike()}>
+                    <FontAwesome name='heart-o' size={24} color={'red'} />
+                </TouchableOpacity>
             }
-        </Text>
       </View>
     )
   }
